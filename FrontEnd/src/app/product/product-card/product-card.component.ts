d35bc1e430b678bc9d6b733061c37d84c6cfbac6
salahbakr <bakrkhaled896@gmail.com>
2023-07-11 04:38:00 +0300

    modified:   FrontEnd/src/app/app.component.html 	modified:   FrontEnd/src/app/product/product-card/product-card.component.html 	modified:   FrontEnd/src/app/product/product-card/product-card.component.ts 	modified:   FrontEnd/src/app/product/product-list/product-list.component.html 	modified:   FrontEnd/src/app/product/product-list/product-list.component.ts

--- FrontEnd/src/app/product/product-card/product-card.component.ts
+++ FrontEnd/src/app/product/product-card/product-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-card',
@@ -6,10 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-card.component.css']
 })
 export class ProductCardComponent {
-  product: any = {
-    "Id": 1,
-    "Name": "Joker Pants",
-    'Type': "pants",
-    "Price": 250
-  }
+  @Input() product: any;
 }
--- FrontEnd/src/app/product/product-list/product-list.component.ts
+++ FrontEnd/src/app/product/product-list/product-list.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
+  products: Array<any> = [
+    {
+    "Id": 1,
+    "Name": "Joker Pants",
+    'Type': "pants",
+    "Price": 250
+    },
+    {
+    "Id": 2,
+    "Name": "Karla House",
+    'Type': "House",
+    "Price": 3521
+    },
+    {
+    "Id": 3,
+    "Name": "Lamborghini",
+    'Type': "Car",
+    "Price": 1000000
+    },
+    {
+    "Id": 4,
+    "Name": "Ferrari",
+    'Type': "Car",
+    "Price": 3600000
+    },
+  ]
 
 }
